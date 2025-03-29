8e5eccab3c063a1c4e1b086a2d5beb201919cc5c
Sven Glauser <75973240+SvenGlauser@users.noreply.github.com>
2025-03-29 16:47:10 +0100

    feature/completion-model (#8)
    - Ajout des infos de création et modification
    - Audit

---
+++ gestion-stock-application/src/main/java/ch/glauser/gestionstock/configuration/AuditorAwareConfiguration.java
@@ -0,0 +1,36 @@
+package ch.glauser.gestionstock.configuration;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+@EnableJpaAuditing
+public class AuditorAwareConfiguration implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+
+        if (principal instanceof UserDetails user) {
+            return Optional.of(user.getUsername());
+        }
+
+        if (principal instanceof AnonymousAuthenticationToken anonymousAuthenticationToken) {
+            return Optional.of(anonymousAuthenticationToken.getPrincipal().toString());
+        }
+
+        if (Objects.isNull(principal)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(principal.toString());
+    }
+}
